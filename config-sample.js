1b7635d04ac50d88d4a74fc02b38ef979cde58b8
Daniel Clayson <danclayson@gmail.com>
2017-11-18 09:32:42 -0800

    Initial Setup

---
+++ config-sample.js
@@ -0,0 +1,10 @@
+var config = {
+    mongoURI: 'mongodb://127.0.0.1:27017/meteor',
+    ddpHost: "127.0.0.1",
+    ddpPort: 3000,
+    ddpEmail: "worker@domain.com",
+    ddpPassword: "password",
+    strandPins: [21,20,16]
+};
+
+module.exports = config;
---
+++ worker.js
@@ -0,0 +1,78 @@
+'use strict';
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+var DDP = require('ddp'),
+    Job = require('meteor-job'),
+    MongoClient = require('mongodb').MongoClient,
+    ddpLogin = require('ddp-login'),
+    Gpio = require('onoff').Gpio,
+    lirc_node = require('lirc_node'),
+    config = require('./config.js'),
+    sleep = require('sleep');
+
+
+var ddp = new DDP({
+    host: config.ddpHost,
+    port: config.ddpPort,
+    use_ejson: true
+})
+
+Job.setDDP(ddp);
+
+
+ddp.connect(function(err) {
+    if (err) throw err;
+    ddpLogin(ddp, {
+        env: "METEOR_TOKEN",
+        method: "account",
+        account: config.ddpEmail,
+        pass: config.ddpPassword,
+        retry: 5,
+        plaintext: false
+    }, function (err, userInfo) {
+        if (err) throw err;
+
+        lirc_node.init();
+        var strands = [];
+        config.strandPins.forEach(pinAssignment=>{
+            strands.push(new Gpio(pinAssignment, 'out'));
+        });
+
+        MongoClient.connect(config.mongoURI, function (err, db) {
+            Job.processJobs('treeJobs', 'commandTree', {concurrency:1, workTimeout: 15 * 1000}, function(job, cb){
+                processTree(lirc_node, strands, job, cb, db);
+            });
+        })
+    })
+});
+
+function processTree(strands, job, cb, db){
+    var actionList = job.data.actionList;
+
+    while(actionList.length){
+        var thisAction = actionList.shift();
+        strands.forEach((strand, index)=>{
+            if(thisAction.strands.indexOf(index) > -1){
+                strand.writeSync(1);
+            } else {
+                strand.writeSync(0);
+            }
+        });
+        lirc_node.irsend.send_once('homestarry-13-colors', thisAction.key);
+        console.log(thisAction);
+        sleep.msleep(thisAction.wait);
+    }
+
+    if(job.data.repeat){
+        db.collection('treeJobs').find({'status': 'ready'}).toArray(function(err, docs){
+            if(docs.length){
+                Job('onumaJobs', 'spaceValueListUpdate', job.data).priority('normal').save();
+            }
+            job.done();
+            cb();
+        })
+    } else {
+        job.done();
+        cb();
+    }
+
+}
